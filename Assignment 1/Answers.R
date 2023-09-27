9ec66daa1654132e6530dfa1856bf803321dde46
RealOkabe <anmolzagrawal@gmail.com>
2023-09-27 10:15:35 -0400

    Completed problem 2
    Removed rows that had 0 impressions and then created categories according to click behavior. Also, started plotting to compare click behavior between males and females less than 20 years of age.

--- Assignment 1/Answers.R
+++ Assignment 1/Answers.R
@@ -1,5 +1,8 @@
 library(dplyr)
 library(ggplot2)
+library(gridExtra)
+
+# Problem 2
 # Read data for 3 days and store into variables
 nyt1 <- read.csv("nyt1.csv")
 nyt2 <- read.csv("nyt2.csv")
@@ -23,6 +26,11 @@ nyt2 <- nyt2[ nyt2$Age != 0, ]
 nyt2 <- addAgeGroup(nyt2)
 nyt3 <- nyt3[ nyt1$Age != 0, ]
 nyt3 <- addAgeGroup(nyt3)
+
+# Removing rows in which Impressions = 0
+nyt1 <- nyt1[ nyt1$Impressions != 0, ]
+nyt2 <- nyt2[ nyt2$Impressions != 0, ]
+nyt3 <- nyt3[ nyt3$Impressions != 0, ]
 # Calculating impressions by AgeGroups
 impressions1 <- aggregate(nyt1$Impressions, by=list(AgeGroup = nyt1$AgeGroup), FUN=sum)
 # Plotting Impressions by AgeGroups
@@ -31,7 +39,7 @@ ggplot(impressions1, aes(x = AgeGroup, y = x, fill = AgeGroup)) +
   labs(title = "Impressions by Age Groups on Day 1",
        x = "Age Group",
        y = "Impressions") +
-       scale_y_continuous(labels = scales::comma) +
+  scale_y_continuous(labels = scales::comma) +
   theme_minimal()
 
 # Day 2 Impressions by AgeGroups
@@ -56,10 +64,7 @@ ggplot(impressions3, aes(x = AgeGroup, y = x, fill = AgeGroup)) +
 
 # If the Impressions are 0, then let's just make the CTR also 0
 getCTR <- function(x) {
-  x %>% mutate(CTR = case_when (
-    Impressions == 0 ~ 0,
-    Impressions != 0 ~ Clicks / Impressions
-  ))
+  x %>% mutate(CTR = Clicks / Impressions)
 }
 nyt1 <- getCTR(nyt1)
 nyt2 <- getCTR(nyt2)
@@ -91,3 +96,71 @@ ggplot(ctr1, aes(x = AgeGroup, y = x, fill = AgeGroup)) +
        y = "CTR") +
   scale_y_continuous(labels = scales::comma) +
   theme_minimal()
+
+# Adding categories based on Clicking behavior
+getClickCategory <- function(x) {
+  x %>% mutate(ClickCategory = case_when(
+    Clicks == 0 ~ "Non Clicker",
+    Clicks < 3 ~ "Low Clicker",
+    Clicks > 3 ~ "High Clicker"
+  ))
+}
+
+nyt1 <- getClickCategory(nyt1)
+nyt2 <- getClickCategory(nyt2)
+nyt3 <- getClickCategory(nyt3)
+
+# Filtering data of <20 year old males and females
+nytMales1 <- nyt1[ nyt1$Gender == 1 & nyt1$Age < 20, ]
+nytMales2 <- nyt2[ nyt2$Gender == 1 & nyt2$Age < 20, ]
+nytMales3 <- nyt3[ nyt3$Gender == 1 & nyt3$Age < 20, ]
+nytFemales1 <- nyt1[ nyt1$Gender == 0 & nyt1$Age < 20, ]
+nytFemales2 <- nyt2[ nyt2$Gender == 0 & nyt2$Age < 20, ]
+nytFemales3 <- nyt3[ nyt3$Gender == 0 & nyt3$Age < 20, ]
+
+
+# Create histograms for CTR in nytMales and nytFemales
+hist_males1 <- ggplot(nytMales1, aes(x = CTR)) +
+  geom_histogram(binwidth = 0.05, fill = "blue", alpha = 0.7) +
+  labs(title = "CTR Distribution for Males on Day 1",
+       x = "CTR",
+       y = "Frequency") +
+  theme_minimal()
+
+hist_females1 <- ggplot(nytFemales1, aes(x = CTR)) +
+  geom_histogram(binwidth = 0.05, fill = "pink", alpha = 0.7) +
+  labs(title = "CTR Distribution for Females on Day 1",
+       x = "CTR",
+       y = "Frequency") +
+  theme_minimal()
+
+hist_males2 <- ggplot(nytMales2, aes(x = CTR)) +
+  geom_histogram(binwidth = 0.05, fill = "blue", alpha = 0.7) +
+  labs(title = "CTR Distribution for Males on Day 2",
+       x = "CTR",
+       y = "Frequency") +
+  theme_minimal()
+
+hist_females2 <- ggplot(nytFemales2, aes(x = CTR)) +
+  geom_histogram(binwidth = 0.05, fill = "pink", alpha = 0.7) +
+  labs(title = "CTR Distribution for Females on Day 2",
+       x = "CTR",
+       y = "Frequency") +
+  theme_minimal()
+
+hist_males3 <- ggplot(nytMales3, aes(x = CTR)) +
+  geom_histogram(binwidth = 0.05, fill = "blue", alpha = 0.7) +
+  labs(title = "CTR Distribution for Males on Day 3",
+       x = "CTR",
+       y = "Frequency") +
+  theme_minimal()
+
+hist_females3 <- ggplot(nytFemales3, aes(x = CTR)) +
+  geom_histogram(binwidth = 0.05, fill = "pink", alpha = 0.7) +
+  labs(title = "CTR Distribution for Females on Day 3",
+       x = "CTR",
+       y = "Frequency") +
+  theme_minimal()
+
+# Display the histograms side by side
+grid.arrange(hist_males1, hist_females1, hist_males2, hist_females2, hist_males3, hist_females3, nrow = 3, ncol = 2)
