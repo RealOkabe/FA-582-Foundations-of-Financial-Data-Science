48b48981fe20b2d5bb32e642ae92a96c28ddb492
RealOkabe <anmolzagrawal@gmail.com>
2023-10-10 16:10:43 -0400

    Completed plots by States and Sectors

--- Assignment 2/Assignment 2.R
+++ Assignment 2/Assignment 2.R
@@ -14,15 +14,31 @@ names(companiesTable)[2] = "Company Name"
 summary(companiesTable)
 
 companiesTable$`Headquarters State` <- sapply(strsplit(companiesTable$`Headquarters Location`, ","), function(x) {
-  
   trimws(x[2])
-  
 }
   )
 
-# Use aggregate to count the number of companies in each state
-stateCounts <- table(companiesTable$`Headquarters State`)
-#stateCounts <- aggregate(`Headquarters State` ~ `Headquarters State`, data = companiesTable, FUN = length)
-print(stateCounts)
-summary(stateCounts)
+companiesTable <- companiesTable[!grepl(";", companiesTable$`Headquarters State`), ]
 
+
+# Use table to count the number of companies in each state
+stateCounts <- table(companiesTable$`Headquarters State`)
+print(stateCounts)
+# Plot the number of companies across states
+barplot(stateCounts, main = "Number of Companies by State", xlab = "State", ylab = "Number of Companies", col = "skyblue", las = 2, cex.names = 0.7)
+
+# Use table to count the number of companies in each GICS Sector
+sectorCounts <- table(companiesTable$`GICS Sector`)
+# Change margins to accomodate Sector names
+par(mar=c(4,7,4,4))
+# Plot Number of Companies by GISC Sector
+barplot(sectorCounts, 
+        main = "Number of Companies by GISC Sector",
+        xlab = "GISC Sector",
+        ylab = "Number of Companies",
+        col = "red",
+        las = 1,
+        cex.names = 0.6,
+        font.axis = 3,
+        horiz = TRUE
+        )
